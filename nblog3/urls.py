364a77b2558b320307702ba324bb4801346b158e
hirokatsuiino <letterbox6212>
2021-01-04 15:49:18 +0900

    [ADD]URLルーツ追加やそのほか更新

--- nblog3/urls.py
+++ nblog3/urls.py
@@ -18,7 +18,13 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
+app_name = 'nblog3'
 
 urlpatterns = [
+    path('api/posts/', views.PostList.as_view(), name='post_list'),
+    path('api/posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('api/categories/', views.CategoryList.as_view(), name='category_list'),
 ]
 
