e39a80745c8408b2dc32d7ad2c07b5f0b7519d6a
hirokatsuiino <letterbox6212>
2021-01-06 17:14:42 +0900

    [MODEF]Ajax→axiosへ変更 [MODEF]コードを変更

--- nblog3/frontend/src/main.js
+++ nblog3/frontend/src/main.js
@@ -2,15 +2,35 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './stroe'
+import axios from 'axios' //追記
+import VueAxios from 'vue-axios' //追記
 
 Vue.config.productionTip = false
 
-Vue.prototype.$http = (url, opts) => fetch(url, opts)
-Vue.prototype.$httpPosts = 'http://127.0.0.1:8100/blog/api/posts/'
-Vue.prototype.$httpCategories = 'http://127.0.0.1:8100/blog/api/categories/'
+Vue.use(VueAxios, axios) //追記
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
+
+// import Vue from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import store from './stroe'
+// import axios from 'axios'
+//
+// Vue.config.productionTip = false
+//
+// Vue.prototype.$http = (url, opts) => fetch(url, opts)
+// // Vue.prototype.$httpPosts = 'http://127.0.0.1:8100/blog/api/posts/'
+// Vue.prototype.$httpPosts = '/blog/api/posts/'
+// Vue.prototype.$httpCategories = 'http://127.0.0.1:8100/blog/api/categories/'
+//
+// new Vue({
+//   router,
+//   store,
+//   axios,
+//   render: h => h(App),
+// }).$mount('#app')
